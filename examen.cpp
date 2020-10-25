0ca34923692cd095d71be13a48af8a8478b3712f
moxie21 <chismara6@gmail.com>
2020-10-25 12:03:46 +0200

    Added destructor and disallowed copy constructor

--- examen.cpp
+++ examen.cpp
@@ -1,22 +1,35 @@
 # include <iostream>
+
 using namespace std;
+
 class Examen{
 private:
-    int rand, nr_trecuti, nr_picati;
+    int rand;
+    int nr_trecuti;
+    int nr_picati;
     Examen& operator=(const Examen&);//prevent the compiler to create the copy assignment operator
+    Examen(const Examen&);//prevents the compiler from creating a default copy constructor
 public:
     Examen(){}
-    Examen(int x, int y, int z)://initializing the fields
+    Examen(int x, int y, int z){//initializing the fields
         rand(x),
         nr_trecuti(y),
         nr_picati(z)
-    {}
-    void getvals(){//return values of fields
-        cout<<rand<<" "<<nr_trecuti<<" "<<nr_picati;
     }
+    ~Examen(){
+        delete rand;
+        delete nr_trecuti;
+        delete nr_picati;
+    }
+    tuple<int, int, int> getvals(){//return values of fields
+        //cout<<rand<<" "<<nr_trecuti<<" "<<nr_picati;
+        return {rand, nr_trecuti, nr_picati};
+    }
+    //TODO: setter
 };
 
 int main(){//test
     Examen e1(1,2,23), e2;
-    e1.getvals();
+    auto [r, t, p] = e1.getvals();
+    cout<<r<<" "<<t<<" "<<p;
 }
